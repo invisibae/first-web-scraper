dfff75a24dd4129518f1fb28df5b32f3bc0d70ac
Greg Morton <gregorymorton96@gmail.com>
2022-02-05 18:33:49 -0500

    commit 4

--- git_scraping.py
+++ git_scraping.py
@@ -6,4 +6,8 @@ response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}) # OUR REQUES
 html = response.content # Response.content
 
 soup = BeautifulSoup(html, features="html.parser")
-print(soup.prettify())
+div = soup.find(id="NO_boxx_row") # This is where it gets tricky.  The page doesn't seem to be a "table" per se, more like a bunch of divs that look like a table.  Tried to adjust accordingly but I'm not 100% on scraping these so I could be wrong.
+
+
+for row in div.find_all('boxx_row'): #tried to get rows with the chunks organized into rows by the div
+    print(row.prettify()) # print a pretty version
